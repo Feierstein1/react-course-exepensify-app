cc1f59e2793b68bf01a31c2d7bae41662d4e7399
Kenny <50640060+Feierstein1@users.noreply.github.com>
2021-08-26 16:29:03 -0400

    FINALLY fixed the stupid issue

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,6 @@
 import Enzyme from 'enzyme'
 import DotEnv from 'dotenv'
-import Adapter from 'enzyme-adapter-react-15'
+import Adapter from 'enzyme-adapter-react-16'
 
 DotEnv.config({ path: '.env.test' })
 
